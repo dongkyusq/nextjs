573ce7d4de361d75fa6fdf4d86a9890dc1804fdf
dongkyusq <hasd789@naver.com>
2024-07-04 12:49:39 +0900

    practice

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function errorO() {
+  return <h1>something broke</h1>;
+}
---
+++ app/about-us/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "About us ",
+  description: "The best movies on the best framework",
+};
+
+export default function AboutUs() {
+  return (
+    <div>
+      <h1>AboutUs Page</h1>
+    </div>
+  );
+}
